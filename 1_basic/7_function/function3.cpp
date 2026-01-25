f6c369dbcbdfe4ff4d1a659039a8b67389e6791b
Het Patel <phet71599@gmail.com>
2026-01-25 23:25:25 +0530

    solving pq.

---
+++ 1_basic/7_function/function3.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+
+//1------------------------------------------
+// int isPrime(int n){
+//     if(n<=1) return 0;
+//     for(int i=2;i*i<=n;i++){
+//         if(n%i==0) return 0;
+//     }
+//     return 1;
+// }
+//------------------------------------------
+
+
+//2------------------------------------------
+int factorial(int n){
+    int fact = 1;
+    for(int i=2;i<=n;i++){
+        fact *= i;
+    }
+    return fact;
+}
+
+int nCr(int n, int r){
+    // nCr = n! / (r! * (n-r)!)
+    return factorial(n) / (factorial(r) * factorial(n - r));
+}
+
+int main(){
+
+    //1------------------------------------------
+    // int num;
+    // cout << "Enter a number: ";
+    // cin >> num;
+    // if(isPrime(num)){
+    //     cout << num << " is a prime number." << endl;
+    // } else {
+    //     cout << num << " is not a prime number." << endl;
+    // }
+
+    
+    //2------------------------------------------
+    //write a function to find binomial coefficient nCr
+    int n, r;
+    cout << "Enter n : ";
+    cin >> n;
+    cout << "Enter r : ";
+    cin >> r;
+
+    cout << "nCr = " << nCr(n, r) << endl;
+
+    return 0;
+}
