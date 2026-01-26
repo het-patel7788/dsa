67742665d7c90660e35e8d13054da1a5b62a2761
Het Patel <phet71599@gmail.com>
2026-01-26 20:34:44 +0530

    function is done.

--- 1_basic/7_function/function3.cpp
+++ 1_basic/7_function/function3.cpp
@@ -14,18 +14,38 @@ using namespace std;
 
 
 //2------------------------------------------
-int factorial(int n){
-    int fact = 1;
-    for(int i=2;i<=n;i++){
-        fact *= i;
-    }
-    return fact;
-}
+// int factorial(int n){
+//     int fact = 1;
+//     for(int i=2;i<=n;i++){
+//         fact *= i;
+//     }
+//     return fact;
+// }
+// int nCr(int n, int r){
+//     // nCr = n! / (r! * (n-r)!)
+//     return factorial(n) / (factorial(r) * factorial(n - r));
+// }
+//-------------------------------------------
 
-int nCr(int n, int r){
-    // nCr = n! / (r! * (n-r)!)
-    return factorial(n) / (factorial(r) * factorial(n - r));
+
+//3------------------------------------------
+int isPrime(int n){
+    if(n<=1) return 0;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0) return 0;
+    }
+    return 1;
 }
+void printPrimesInRange(int low, int high){
+    for(int num=low; num<=high; num++){
+        if(isPrime(num)){
+            cout << num << " ";
+        }
+    }
+    cout << endl;
+}
+//-------------------------------------------
+
 
 int main(){
 
@@ -41,14 +61,28 @@ int main(){
 
     
     //2------------------------------------------
-    //write a function to find binomial coefficient nCr
-    int n, r;
-    cout << "Enter n : ";
-    cin >> n;
-    cout << "Enter r : ";
-    cin >> r;
+    // //write a function to find binomial coefficient nCr
+    // int n, r;
+    // cout << "Enter n : ";
+    // cin >> n;
+    // cout << "Enter r : ";
+    // cin >> r;
 
-    cout << "nCr = " << nCr(n, r) << endl;
+    // cout << "nCr = " << nCr(n, r) << endl;
+    //-------------------------------------------
+
+
+    //3------------------------------------------
+    // print all prime numbers in a given range using function
+    int low, high;
+    cout << "Enter lower bound: ";
+    cin >> low;
+    cout << "Enter upper bound: ";
+    cin >> high;
+
+    cout << "Prime numbers between " << low << " and " << high << " are: ";
+    printPrimesInRange(low, high);
+    //-------------------------------------------
 
     return 0;
 }
---
+++ 1_basic/7_function/function4.cpp
@@ -0,0 +1,86 @@
+// #include <iostream>
+// using namespace std;
+
+
+// int sizeOfNum(int n){
+//     int size = 0;
+//     while(n > 0){
+//         size += 1;
+//         n /= 10;
+//     }
+//     return size;
+// }
+
+// bool isPalindrome(int n){
+//     int size = sizeOfNum(n);
+//     int reversedNum = 0;
+//     int originalNum = n;
+//     for(int i = 0; i < size; i++){
+//         reversedNum = reversedNum * 10 + (originalNum % 10);
+//         originalNum /= 10;
+//     }
+//     return (reversedNum == n);
+// }
+
+// int main(){
+    
+//     //1. check is the number is palindrome or not
+//     int n;
+//     cout << "Enter the number n: ";
+//     cin >> n;
+
+//     if(isPalindrome(n)){
+//         cout << n << " is a palindrome number." << endl;
+//     } else {
+//         cout << n << " is not a palindrome number." << endl;
+//     }
+    
+//     return 0;
+// }
+
+
+
+
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int sumOfDigit(int n){
+//     int sum = 0;
+//     while(n > 0){
+//         sum += n % 10;
+//         n /= 10;
+//     }
+//     return sum;
+// }
+
+// int main(){
+
+//     int n;
+//     cout << "Enter the value of n: ";
+//     cin >> n;
+
+//     int s = sumOfDigit(n);
+//     cout << "The total is :" << s << endl;
+
+//     return 0;
+// }
+
+
+
+
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+    
+    int a, b;
+
+    return 0;
+}
