27f3141b74de96ec948e08aad2c7b5a13f2da58f
Het Patel <phet71599@gmail.com>
2026-02-17 23:23:56 +0530

    solving..

---
+++ 2_array/questions/q25.cpp
@@ -0,0 +1,93 @@
+//leetcode 11
+//Container With Most Water   
+/*
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.  */
+
+
+
+
+//brute force approch
+// #include <iostream>
+// #include <vector>
+// #include <algorithm> 
+// using namespace std;
+
+// int mostWater(vector<int> vec){
+//     int n = vec.size();
+//     int maxWater = 0;
+
+//     for(int i = 0; i < n; i++){
+        
+//         for(int j = i+1; j < n; j++){
+            
+//             int height = min(vec[i], vec[j]);
+            
+//             int width = j - i;
+            
+//             int currWater = height * width;
+            
+//             maxWater = max(maxWater, currWater);
+//         }
+//     }
+//     return maxWater;
+// }
+
+// int main(){
+
+//     vector<int> vec = {1,8,6,2,5,4,8,3,7};
+
+//     int ans = mostWater(vec);
+//     cout << "Most water storing is : " << ans;
+
+//     return 0;
+// }
+
+
+
+
+
+//optimize approch(two pointer)
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int mostWater(vector<int> vec){
+    int size = vec.size();
+    int s = 0;
+    int e = size-1;
+    int maxWater = 0;   
+    int currWater = 0;
+
+    while(s<e){
+        int hight = min(vec[s],vec[e]);
+        int width = e - s;
+        currWater = hight * width;
+        maxWater = max(maxWater, currWater);
+        if(vec[s] < vec[e]){
+            s++;
+        }
+        else{
+            e--;
+        }
+    }
+
+    return maxWater;
+}
+
+int main(){
+
+    vector<int> vec = {1,8,6,2,5,4,8,3,7};
+
+    int ans = mostWater(vec);
+    cout << "Most water storing is : " << ans << endl;
+    
+    return 0;
+    
+}
