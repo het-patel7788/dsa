0466d698d51f31d8a17d501434e45afea1ce4b36
Het Patel <phet71599@gmail.com>
2026-03-30 20:38:48 +0530

    solving..

---
+++ 4_divide_and_conquer/questions/que3.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int countInRange(vector<int>& nums, int num, int left, int right) {
+    int count = 0;
+    for (int i = left; i <= right; i++) {
+        if (nums[i] == num) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int majorityElementRec(vector<int>& nums, int left, int right) {
+    if (left == right) {
+        return nums[left];
+    }
+
+    int mid = left + (right - left) / 2;
+    int leftMajority = majorityElementRec(nums, left, mid);
+    int rightMajority = majorityElementRec(nums, mid + 1, right);
+
+    if (leftMajority == rightMajority) {
+        return leftMajority;
+    }
+
+    int leftCount = countInRange(nums, leftMajority, left, right);
+    int rightCount = countInRange(nums, rightMajority, left, right);
+
+    if (leftCount > rightCount) {
+        return leftMajority;
+    } else {
+        return rightMajority;
+    }
+}
+
+int majorityElement(vector<int>& nums) {
+    return majorityElementRec(nums, 0, nums.size() - 1);
+}
+
+int main() {
+    vector<int> nums = {2,3,3,3,3,3,2};
+    
+    cout << majorityElement(nums) << "\n";
+    
+    return 0;
+}
